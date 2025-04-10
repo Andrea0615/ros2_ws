7d88cde871cc9edd759849dacf0c92e113beee5b
Andrea0615 <a01641193@tec.mx>
2025-04-09 18:33:11 -0600

    Proyect init

---
+++ build/catkin_generated/order_packages.py
@@ -0,0 +1,5 @@
+# generated from catkin/cmake/template/order_packages.context.py.in
+source_root_dir = '/home/andrea/robot_ws/src'
+whitelisted_packages = ''.split(';') if '' != '' else []
+blacklisted_packages = ''.split(';') if '' != '' else []
+underlay_workspaces = '/home/andrea/catkin_ws/devel;/opt/ros/melodic'.split(';') if '/home/andrea/catkin_ws/devel;/opt/ros/melodic' != '' else []
